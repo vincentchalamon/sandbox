47278088ee85ce23590bb1af8744a9db3acfa7a1
Vincent CHALAMON <vincentchalamon@gmail.com>
2014-04-28 19:06:35 +0200

    Update phpdoc

--- src/My/Bundle/CmsBundle/Tests/Admin/MenuAdminTest.php
+++ src/My/Bundle/CmsBundle/Tests/Admin/MenuAdminTest.php
@@ -3,6 +3,8 @@
 /*
  * This file is part of the Sandbox package.
  *
+ * (c) Vincent Chalamon <vincentchalamon@gmail.com>
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
