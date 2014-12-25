bb760bf58314f4e40a45cbf809ef257457dd76c5
Vincent Chalamon <vincentchalamon@gmail.com>
2014-12-25 22:26:57 +0100

    Update without i18n

--- src/My/Bundle/CmsBundle/Entity/Article.php
+++ src/My/Bundle/CmsBundle/Entity/Article.php
@@ -35,4 +35,37 @@ class Article extends BaseArticle
         parent::__construct();
         $this->categories = new ArrayCollection();
     }
+
+    /**
+     * Add category
+     *
+     * @param Category category
+     * @return Article
+     */
+    public function addCategory(Category $category)
+    {
+        $this->categories[] = $category;
+
+        return $this;
+    }
+
+    /**
+     * Remove category
+     *
+     * @param Category $category
+     */
+    public function removeCategory(Category $category)
+    {
+        $this->categories->removeElement($category);
+    }
+
+    /**
+     * Get categories
+     *
+     * @return ArrayCollection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
 }
